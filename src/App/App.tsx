e82c117896d13b4651da2347849840ca52a51b6e
iamxenna <vanya.tar@gmail.com>
2022-10-25 23:32:27 +0300

    new updates

---
+++ src/App/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css';
+import Header from "../Header/Header";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Main from "../Main/Main";
+
+enum Links{
+    DEFAULT = "https://vk.com/video_ext.php?oid=-211941733&id=456239177&hash=320cbbbc09eb7482",
+    RUSSIAN = "https://vk.com/video_ext.php?oid=-211941733&id=456239177&hash=320cbbbc09eb7482",
+    INTER = "https://vk.com/video_ext.php?oid=-211941733&id=456239178&hash=fdd4cc8757face86",
+    ENGLISH = "https://vk.com/video_ext.php?oid=-211941733&id=456239179&hash=a3e3f73e33a58ff3",
+    COMMUNITY = "https://vk.com/video_ext.php?oid=-211941733&id=456239179&hash=a3e3f73e33a58ff3",
+}
+
+function App() {
+  return (
+    <>
+      <Header/>
+        <BrowserRouter>
+            <Routes>
+                <Route path={"/"} element={<Main link={Links.DEFAULT} title={"2022 Japan Grand Prix"}/>}></Route>
+                <Route path={"/russian"} element={<Main link={Links.RUSSIAN} title={"2022 Japan Grand Prix"}/>}></Route>
+                <Route path={"/intershum"} element={<Main link={Links.INTER} title={"2022 Japan Grand Prix"}/>}></Route>
+                <Route path={"/english"} element={<Main link={Links.ENGLISH} title={"2022 Japan Grand Prix"}/>}></Route>
+                <Route path={"/community"} element={<Main link={Links.COMMUNITY} title={"2022 Japan Grand Prix"}/>} ></Route>
+                <Route path={"/support"} element={""}></Route>
+            </Routes>
+        </BrowserRouter>
+    </>
+  );
+}
+export default App;
--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,32 +1,34 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './Header.css'
 import {Button, Container, Image, Nav} from "react-bootstrap";
+import Logo from "../img/2418779.svg"
 
 function Header() {
     return (
         <div>
-            <Nav className={"navbar navbar-expand-lg navbar-dark bg-dark"}>
+            <Nav className={"navbar navbar-expand-lg navbar-dark bg-danger"}>
                 <Container>
-                    <Image className={"nav-item"} src={"https://upload.wikimedia.org/wikipedia/commons/3/33/F1.svg"} width="150" height="50" alt=""/>
+                    <Image className={"nav-item img"} src={Logo} width="150" height="50" alt=""/>
                     <Button className={"navbar-toggler bg-dark"} type={"button"} data-bs-toggle={"collapse"}
                         data-bs-target={"#navContent"} aria-controls={"#navContent"} aria-expanded={"false"} aria-label={"Переключатель навигации"}>
                         <span className={"navbar-toggler-icon"}></span>
                     </Button>
                     <div className={"collapse navbar-collapse"} id={"navContent"}>
-                        <ul className={"navbar-nav mb-0 mb-lg-0"}>
+                        <ul className={"navbar-nav mb-0 mb-lg-0 asd"} id={"bar"}>
                             <li className={"nav-item"}>
-                                <a className={"nav-link active text-light"} aria-current={"page"} href={"/russian"}>Русский</a>
+                                <a className={"nav-link active text-light ms-lg-3"} aria-current={"page"} href={"/russian"}>Русский</a>
                             </li>
                             <li className={"nav-item"}>
-                                <a className={"nav-link active text-light"} aria-current={"page"} href={"/intershum"}>Интершум</a>
+                                <a className={"nav-link active text-light ms-lg-3"} aria-current={"page"} href={"/intershum"}>Интершум</a>
                             </li>
                             <li className={"nav-item"}>
-                                <a className={"nav-link active text-light"} aria-current={"page"} href={"/english"}>English</a>
+                                <a className={"nav-link active text-light ms-lg-3"} aria-current={"page"} href={"/english"}>English</a>
                             </li>
                             <li className={"nav-item"}>
-                                <a className={"nav-link active text-light"} aria-current={"page"} href={"/community"}>Сообщество</a>
+                                <a className={"nav-link active text-light ms-lg-3"} aria-current={"page"} href={"/community"}>Сообщество</a>
                             </li>
                             <li className={"nav-item"}>
-                                <a className={"nav-link active text-light"} aria-current={"page"} href={"/support"}>Поддержать</a>
+                                <a className={"nav-link active text-light ms-lg-3"} aria-current={"page"} href={"/support"}>Поддержать</a>
                             </li>
                         </ul>
                     </div>
