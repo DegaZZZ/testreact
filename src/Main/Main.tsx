e4590a23c287b2e4bc59448b44621e44e07086e4
iamxenna <vanya.tar@gmail.com>
2022-10-25 21:53:29 +0300

    asdasd

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -1,20 +1,24 @@
 import './Main.css'
 
 let link: string = "https://vk.com/video_ext.php?oid=-211941733&id=456239158&hash=40159766b0115fd1";
-if (window.location.href === "http://localhost:3000/russian"){
-    link = "https://vk.com/video_ext.php?oid=-211941733&id=456239177&hash=320cbbbc09eb7482";
-}
 
-else if(window.location.href === "http://localhost:3000/intershum"){
-    link = "https://vk.com/video_ext.php?oid=-211941733&id=456239178&hash=fdd4cc8757face86";
-}
+function broadcastLink(link: string): string {
+    if (window.location.href === "http://localhost:3000/russian"){
+        return "https://vk.com/video_ext.php?oid=-211941733&id=456239177&hash=320cbbbc09eb7482";
+    }
 
-else if(window.location.href === "http://localhost:3000/english"){
-    link = "https://vk.com/video_ext.php?oid=-211941733&id=456239179&hash=a3e3f73e33a58ff3";
-}
+    else if(window.location.href === "http://localhost:3000/intershum"){
+        return "https://vk.com/video_ext.php?oid=-211941733&id=456239178&hash=fdd4cc8757face86";
+    }
 
-else if(window.location.href === "http://localhost:3000/community"){
-    link = "https://vk.com/video_ext.php?oid=-211941733&id=456239179&hash=a3e3f73e33a58ff3";
+    else if(window.location.href === "http://localhost:3000/english"){
+        return "https://vk.com/video_ext.php?oid=-211941733&id=456239179&hash=a3e3f73e33a58ff3";
+    }
+
+    else if(window.location.href === "http://localhost:3000/community"){
+        return "https://vk.com/video_ext.php?oid=-211941733&id=456239179&hash=a3e3f73e33a58ff3";
+    }
+    return link;
 }
 
 function Main() {
@@ -22,7 +26,7 @@ function Main() {
         <div className="container-fluid main-container d-flex justify-content-center overflow-hidden px-0">
             <div className="player-wrapper" id="split-player-formula">
                 <iframe className="iframe"
-                        src={link}
+                        src={broadcastLink(link)}
                         allow="autoplay; encrypted-media; fullscreen; picture-in-picture">
                 </iframe>
                 <h1>2022 Japan Grand Prix</h1>
